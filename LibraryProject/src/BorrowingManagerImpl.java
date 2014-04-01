924cf0371394c720ff6bd95ec5a251c889cbf3f8
Dudys <jan.duda27@gmail.com>
2014-04-01 13:11:49 +0200

    přidání BorrowingManagerImpl

---
+++ LibraryProject/src/BorrowingManagerImpl.java
@@ -0,0 +1,46 @@
+
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Honza
+ */
+public class BorrowingManagerImpl implements BorrowingManager{
+
+    @Override
+    public void addBorrowing(Borrowing borrowing) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateBorrowing(Borrowing borrowing) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteBorrowing(Borrowing borrowing) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Borrowing> findAllBorrowing() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Borrowing findBorrowingById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Borrowing> findBorrowingByReader(Reader reader) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
